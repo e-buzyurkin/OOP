7fe9355b26ea7c849818f82d7c26217d9fcc00fe
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2023-09-28 00:28:02 +0700

    Task_1_1_2 Initial commit

---
+++ Task_1_1_2/src/test/java/ru/nsu/buzyurkin/PolynomialTest.java
@@ -0,0 +1,87 @@
+package ru.nsu.buzyurkin;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class PolynomialTest {
+    @Test
+    void test_init() {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+    }
+
+    @Test
+    void test_plus() {
+        Polynomial p1 = new Polynomial(new int[] {10, 5, 3, 7, 11});
+        Polynomial p2 = new Polynomial(new int[] {5, 18, 5, 3, 7, 11});
+
+        assertEquals(p1.plus(p2).toString(), "11x^5 + 18x^4 + 10x^3 + 8x^2 + 23x + 15");
+    }
+
+    @Test
+    void test_minus() {
+        Polynomial p1 = new Polynomial(new int[] {10, -5, 3, 7, 11});
+        Polynomial p2 = new Polynomial(new int[] {5, 18, 5, 3, -7, 11});
+
+        assertEquals(p1.minus(p2).toString(), "-11x^5 + 18x^4 + 4x^3 - 2x^2 - 23x + 5");
+    }
+
+    @Test
+    void test_multiply() {
+        Polynomial p1 = new Polynomial(new int[] {10, -5, 3, 7, 11, -100, 13, 53});
+        Polynomial p2 = new Polynomial(new int[] {5, 18, 5, 3, -7, 11});
+
+        assertEquals(p1.times(p2).toString(), p2.times(p1).toString());
+        assertEquals(p1.times(p2).toString(), "583x^12 - 228x^11 - 1032x^10 + 1125x^9 + 719x^8 + 16x^7 - 1735x^6 - " +
+                "113x^5 + 111x^4 + 94x^3 - 25x^2 + 155x + 50");
+    }
+
+    @Test
+    void test_differentiation() {
+        Polynomial p1 = new Polynomial(new int[] {10, -5, 3, 7, 11, -10, 13, 1});
+
+        assertEquals(p1.differentiate(10).toString(), "0");
+        assertEquals(p1.differentiate(5).toString(), "2520x^2 + 9360x - 1200");
+        assertEquals(p1.differentiate(0).toString(), "x^7 + 13x^6 - 10x^5 + 11x^4 + 7x^3 + 3x^2 - 5x + 10");
+    }
+
+    @Test
+    void test_evaluation() {
+        Polynomial p1 = new Polynomial(new int[] {10, -5, 3, 7, 11});
+
+        assertEquals(p1.evaluate(0), 10);
+        assertEquals(p1.evaluate(3), 1102);
+        assertEquals(p1.evaluate(-1.5), 56.3125);
+    }
+
+    @Test
+    void test_equality() {
+        Polynomial p1 = new Polynomial(new int[] {10, -5, 3});
+        assertTrue(p1.isEqual(p1));
+
+        Polynomial p2 = new Polynomial(new int[] {10, -5, 3, 0, 0});
+        assertTrue(p1.isEqual(p2));
+        assertTrue(p2.isEqual(p1));
+
+        Polynomial p3 = new Polynomial(new int[] {0, 10, 3, 7, 11});
+        assertFalse(p1.isEqual(p3));
+        assertFalse(p3.isEqual(p1));
+
+        Polynomial p4 = new Polynomial(new int[] {103, -10, 40, 0, 10, 3, 7, 11});
+        assertFalse(p1.isEqual(p4));
+        assertFalse(p4.isEqual(p1));
+    }
+
+    @Test
+    void test_stringRepresentation() {
+        Polynomial p1 = new Polynomial(new int[] {0, 0, 0, 0,});
+        assertEquals(p1.toString(), "0");
+
+        Polynomial p2 = new Polynomial(new int[] {103, -10, 40, 0, 10, 3, 7, -11});
+        assertEquals(p2.toString(), "-11x^7 + 7x^6 + 3x^5 + 10x^4 + 40x^2 - 10x + 103");
+
+        Polynomial p3 = new Polynomial(new int[] {0, 1, 1, 1, 1, 1, 1, 1});
+        assertEquals(p3.toString(), "x^7 + x^6 + x^5 + x^4 + x^3 + x^2 + x");
+
+        Polynomial p4 = new Polynomial(new int[] {1203});
+        assertEquals(p4.toString(), "1203");
+    }
+}
