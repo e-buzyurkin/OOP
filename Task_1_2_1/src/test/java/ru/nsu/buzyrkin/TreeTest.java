85e3312dfd0025b36f829b948d1ccf5cfd973636
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2023-10-12 00:29:11 +0700

    Task_1_2_1 initial commit

---
+++ Task_1_2_1/src/test/java/ru/nsu/buzyrkin/TreeTest.java
@@ -0,0 +1,262 @@
+package ru.nsu.buzyrkin;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.nsu.buzyurkin.Tree;
+
+import java.util.*;
+
+public class TreeTest {
+
+    /**
+     * Test case for checking equality between two trees with different child orders.
+     */
+    @Test
+    void test_equality1() {
+        Tree<String> tree = new Tree<>("R1");
+        tree.addChild("A");
+        tree.addChild("B");
+
+        Tree<String> tree1 = new Tree<>("R1");
+        tree1.addChild("B");
+        tree1.addChild("A");
+
+        assertTrue(tree.equals(tree1));
+    }
+
+    /**
+     * Test case for checking equality between two complex trees with different child orders.
+     */
+    @Test
+    void test_equality2() {
+        Tree<String> tree = new Tree<>("R1");
+        var a1 = tree.addChild("A");
+        var b1 = tree.addChild("B");
+        a1.addChild("C");
+        a1.addChild("D");
+        b1.addChild("E");
+        b1.addChild("F");
+
+
+        Tree<String> tree1 = new Tree<>("R1");
+        var a2 = tree1.addChild("A");
+        var b2 = tree1.addChild("B");
+        a2.addChild("D");
+        b2.addChild("F");
+        a2.addChild("C");
+        b2.addChild("E");
+
+        assertTrue(tree.equals(tree1));
+    }
+
+    /**
+     * Test case for checking inequality between two trees after a node removal.
+     */
+    @Test
+    void test_equality3() {
+        Tree<String> tree = new Tree<>("R1");
+        var a = tree.addChild("A");
+        var b = a.addChild("B");
+        Tree<String> subtree = new Tree<>("R2");
+        subtree.addChild("C");
+        subtree.addChild("D");
+        tree.addChild(subtree);
+        a.remove();
+
+        Tree<String> tree1 = new Tree<>("R1");
+        tree1.addChild("A");
+        var c = tree1.addChild("R2");
+        c.addChild("D");
+        c.addChild("C");
+
+        assertNotEquals(tree, tree1);
+    }
+
+    /**
+     * Test case for checking equality between two trees after a node removal.
+     */
+    @Test
+    void test_equality4() {
+        Tree<String> tree = new Tree<>("R1");
+        var a = tree.addChild("A");
+        var b = a.addChild("B");
+        Tree<String> subtree = new Tree<>("R2");
+        subtree.addChild("C");
+        subtree.addChild("D");
+        tree.addChild(subtree);
+        a.remove();
+
+        Tree<String> tree1 = new Tree<>("R1");
+        var c = tree1.addChild("R2");
+        c.addChild("D");
+        c.addChild("C");
+
+        assertEquals(tree, tree1);
+    }
+
+    /**
+     * Test case for iterating the entire tree using Depth-First Search (DFS) order.
+     */
+    @Test
+    void test_iteratorWholeTreeDFS() {
+        Tree<String> tree = new Tree<>("R1");
+        var a1 = tree.addChild("A");
+        var b1 = tree.addChild("B");
+        a1.addChild("C");
+        a1.addChild("D");
+        b1.addChild("E");
+        b1.addChild("F");
+
+        Iterator<String> iteratorDFS = tree.iterator();
+        int size = 0;
+        while (iteratorDFS.hasNext()) {
+            size++;
+            iteratorDFS.next();
+        }
+
+        assertEquals(7, size);
+    }
+
+    /**
+     * Test case for iterating the entire tree using Breadth-First Search (BFS) order.
+     */
+    @Test
+    void test_iteratorWholeTreeBFS() {
+        Tree<String> tree = new Tree<>("R1");
+        var a1 = tree.addChild("A");
+        var b1 = tree.addChild("B");
+        a1.addChild("C");
+        a1.addChild("D");
+        b1.addChild("E");
+        b1.addChild("F");
+
+        Iterator<String> iteratorBFS = tree.iteratorBFS();
+        int size = 0;
+        while (iteratorBFS.hasNext()) {
+            size++;
+            iteratorBFS.next();
+        }
+
+        assertEquals(7, size);
+    }
+
+    /**
+     * Test case for checking the order of elements when iterating the tree using DFS.
+     */
+    @Test
+    void test_iteratorRightOrderDFS() {
+        Tree<String> tree = new Tree<>("R1");
+        var a1 = tree.addChild("A");
+        var b1 = tree.addChild("B");
+        a1.addChild("C");
+        a1.addChild("D");
+        b1.addChild("E");
+        b1.addChild("F");
+
+        Iterator<String> iteratorDFS = tree.iterator();
+        int step = 3;
+        String val = "";
+        while (step > 0) {
+            step--;
+            if (iteratorDFS.hasNext()) {
+                val = iteratorDFS.next();
+            }
+        }
+
+        assertEquals("C", val);
+    }
+
+    /**
+     * Test case for ensuring no exceptions are thrown when iterating after tree modification using DFS.
+     */
+    @Test
+    public void test_iteratorNoExceptionDFS() {
+        Tree<String> tree = new Tree<>("R1");
+        var a1 = tree.addChild("A");
+        var b1 = tree.addChild("B");
+        a1.addChild("C");
+        a1.addChild("D");
+        b1.addChild("E");
+        b1.addChild("F");
+
+        Iterator<String> iteratorA = a1.iterator();
+
+        assertDoesNotThrow(() -> {
+            b1.addChild("other");
+            while (iteratorA.hasNext()) {
+                iteratorA.next();
+            }
+        });
+    }
+
+    /**
+     * Test case for checking exceptions when iterating after tree modification using DFS.
+     */
+    @Test
+    public void test_iteratorExceptionDFS() {
+        Tree<String> tree = new Tree<>("R1");
+        var a1 = tree.addChild("A");
+        var b1 = tree.addChild("B");
+        a1.addChild("C");
+        a1.addChild("D");
+        b1.addChild("E");
+        b1.addChild("F");
+
+        Iterator<String> iteratorA = b1.iterator();
+
+        assertThrows(ConcurrentModificationException.class, () -> {
+            b1.addChild("other");
+            while (iteratorA.hasNext()) {
+                iteratorA.next();
+            }
+        });
+    }
+
+    /**
+     * Test case for ensuring no exceptions are thrown when iterating after tree modification using BFS.
+     */
+    @Test
+    public void test_iteratorNoExceptionBFS() {
+        Tree<String> tree = new Tree<>("R1");
+        var a1 = tree.addChild("A");
+        var b1 = tree.addChild("B");
+        a1.addChild("C");
+        a1.addChild("D");
+        b1.addChild("E");
+        b1.addChild("F");
+
+        Iterator<String> iteratorA = a1.iteratorBFS();
+
+        assertDoesNotThrow(() -> {
+            b1.addChild("other");
+            while (iteratorA.hasNext()) {
+                iteratorA.next();
+            }
+        });
+    }
+
+    /**
+     * Test case for checking exceptions when iterating after tree modification using BFS.
+     */
+    @Test
+    public void test_iteratorExceptionBFS() {
+        Tree<String> tree = new Tree<>("R1");
+        var a1 = tree.addChild("A");
+        var b1 = tree.addChild("B");
+        a1.addChild("C");
+        a1.addChild("D");
+        b1.addChild("E");
+        b1.addChild("F");
+
+        Iterator<String> iteratorA = b1.iteratorBFS();
+
+        assertThrows(ConcurrentModificationException.class, () -> {
+            b1.addChild("other");
+            while (iteratorA.hasNext()) {
+                iteratorA.next();
+            }
+        });
+    }
+}
