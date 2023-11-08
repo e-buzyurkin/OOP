81ef6f6a6eb8ffefe48b905c0628aba2d7b46230
e-buzyurkin <e-buzyurkin@users.noreply.github.com>
2023-11-08 16:53:02 +0000

    Deploying to gh-pages from @ e-buzyurkin/OOP@92a28435643d0db814b6c907e321948141444d0c 🚀

---
+++ Task_1_3_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.buzyurkin","c":"SubstringInFile","l":"findSubstring(String)","url":"findSubstring(java.lang.String)"},{"p":"ru.nsu.buzyurkin","c":"SubstringInFile","l":"SubstringInFile(String)","url":"%3Cinit%3E(java.lang.String)"}]
