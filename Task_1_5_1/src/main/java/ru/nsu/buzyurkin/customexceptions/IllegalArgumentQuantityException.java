838fa573d6be2f57f7d8adcb5a20bfee6ba7e86f
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2023-12-12 22:11:38 +0700

    added description to the custom exceptions

--- Task_1_5_1/src/main/java/ru/nsu/buzyurkin/customexceptions/IllegalArgumentQuantityException.java
+++ Task_1_5_1/src/main/java/ru/nsu/buzyurkin/customexceptions/IllegalArgumentQuantityException.java
@@ -6,4 +6,7 @@ package ru.nsu.buzyurkin.customexceptions;
  * to signal that the quantity of arguments provided for a mathematical operation is invalid.
  */
 public class IllegalArgumentQuantityException extends Exception {
+    public IllegalArgumentQuantityException() {
+        super("Wrong quantity of arguments passed as an input");
+    }
 }
--- Task_1_5_1/src/main/java/ru/nsu/buzyurkin/customexceptions/IllegalExpressionException.java
+++ Task_1_5_1/src/main/java/ru/nsu/buzyurkin/customexceptions/IllegalExpressionException.java
@@ -6,4 +6,9 @@ package ru.nsu.buzyurkin.customexceptions;
  * to signal that a mathematical expression is not valid or well-formed.
  */
 public class IllegalExpressionException extends Exception {
+    public IllegalExpressionException() {
+        super("Wrong expression passes as an input");
+    }
+
+
 }
