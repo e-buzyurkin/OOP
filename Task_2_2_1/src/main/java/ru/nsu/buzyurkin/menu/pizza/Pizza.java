c9602baec1cae742d776f16725569b5c2b179303
Evgeniy Buzyurkin <e.buzyurkin@g.nsu.ru>
2024-04-10 18:49:46 +0700

    trying to fix build error 2

--- Task_2_2_1/src/main/java/ru/nsu/buzyurkin/menu/pizza/Pizza.java
+++ Task_2_2_1/src/main/java/ru/nsu/buzyurkin/menu/pizza/Pizza.java
@@ -2,12 +2,12 @@ package ru.nsu.buzyurkin.menu.pizza;
 
 import ru.nsu.buzyurkin.menu.MenuItem;
 
-///**
-// * Represents a pizza item on the menu.
-// * @param pizzaMenu the pizza menu associated with this pizza item
-// * @param pizzaSize the size of the pizza
-// * @param pizzaCrust the crust type of the pizza
-// */
+/**
+ * Represents a pizza item on the menu.
+ * @param pizzaMenu the pizza menu associated with this pizza item
+ * @param pizzaSize the size of the pizza
+ * @param pizzaCrust the crust type of the pizza
+ */
 public record Pizza(PizzaMenu pizzaMenu,
                     PizzaSize pizzaSize,
                     PizzaCrust pizzaCrust) implements MenuItem {}
